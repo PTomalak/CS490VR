83a35918d174094a6eda5270fef402c050b37507
Patryk Tomlak <patryktomalak.email@gmail.com>
2023-05-03 01:24:22 -0400

    Update RemoveHelper.cs

--- Unity_proj/CS490VR/Assets/CS490VR/Scripts/VR/RemoveHelper.cs
+++ Unity_proj/CS490VR/Assets/CS490VR/Scripts/VR/RemoveHelper.cs
@@ -16,21 +16,12 @@ public class RemoveHelper : MonoBehaviour
 
         GameObject newBlock = Instantiate(prefab, blockPosition, Quaternion.identity);
         newBlock.transform.localScale = Vector3.one * 0.05f;
+        Destroy(newBlock, 1f);
 
         RaycastHit[] hit = Physics.RaycastAll(blockPosition, Vector3.up, 1.5f, layermask);
         // get the nearest raycast by sorting through them
         if (hit.Length > 0)
         {
-            // wire example
-            bool isWire = (hit[0].transform.gameObject.name == "Wire Controller");
-            Vector3 v = hit[0].point;
-            int id_wire = blockManager.GetWireAtGlobalPosition(blockPosition.x, blockPosition.y, blockPosition.z);
-            if (id_wire != 0)
-            {
-                // this is a wire
-                blockManager.ClientRemoveBlock(id_wire);
-            }
-
             // for normal blocks
             IDataLoader dl = hit[0].transform.GetComponent<IDataLoader>();
             if (dl != null)
